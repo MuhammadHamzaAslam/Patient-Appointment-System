0339d154602ff6807645a0c1eba03b376270088a
Hamza <153912992+MuhammadHamzaAslam@users.noreply.github.com>
2024-11-11 19:22:34 +0500

    debugging

--- src/app/api/request/route.js
+++ src/app/api/request/route.js
@@ -14,17 +14,37 @@ export async function POST(request) {
   await connectDB();
   try {
     let obj = await request.json();
-    let newRequest = await new RequestModal({ ...obj });
-    newRequest = await newRequest.save();
-    return Response.json(
-      {
-        error: false,
-        msg: "Doctor Resquest received Successfully",
-        user: newRequest,
-      },
-      { status: 201 }
-    );
+    let userRequestedBefore = await RequestModal.findOne({
+      user: obj.user,
+    });
+    if (userRequestedBefore) {
+      return Response.json(
+        {
+          error: true,
+          msg: "You Have Already Applied For Doctor",
+        },
+        { status: 401 }
+      );
+    } else {
+      let newRequest = await new RequestModal({ ...obj });
+      newRequest = await newRequest.save();
+      return Response.json(
+        {
+          error: false,
+          msg: "Doctor Resquest received Successfully",
+          user: newRequest,
+        },
+        { status: 201 }
+      );
+    }
   } catch (error) {
     console.log(error);
+    return Response.json(
+      {
+        error: true,
+        msg: "Check Console For Errors",
+      },
+      { status: 500 }
+    );
   }
 }
