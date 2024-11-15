40ae4adcbb91d579c8a3978b69c8b7a237bb8acb
Hamza <153912992+MuhammadHamzaAslam@users.noreply.github.com>
2024-11-15 15:36:53 +0500

    debugging

--- src/actions/request.js
+++ src/actions/request.js
@@ -1,8 +1,12 @@
 export async function addRequest(data) {
-  let add = await fetch(`http://localhost:3000/api/request`, {
+  console.log("addbase url =>", process.env.NEXT_PUBLIC_BASE_URL);
+  let add = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/request`, {
     method: "POST",
     body: JSON.stringify(data),
   });
+
   add = add.json();
+  console.log("add =>", add);
+
   return add;
 }
--- src/app/admin/request/page.js
+++ src/app/admin/request/page.js
@@ -5,7 +5,7 @@ import { redirect } from 'next/navigation';
 export default async function AdminRequest() {
     const session = await auth()
     // console.log("session =>" , session);
-    if(session?.user?.role !== "admin") redirect('/')
+    if(!session && session?.user?.role !== "admin") redirect('/')
   return (
     <section>
       <h1 className='flex justify-center items-center min-h-screen text-5xl font-bold'>Doctor Request</h1>
